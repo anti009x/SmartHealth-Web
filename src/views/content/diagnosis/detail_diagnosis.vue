b392739b915c6e78e07efd7cb33a0d790a18364a
anti009x <riskisuleman76@gmail.com>
2023-10-31 10:25:26 +0700

    Perbaharuan Design V1

--- src/views/content/diagnosis/detail_diagnosis.vue
+++ src/views/content/diagnosis/detail_diagnosis.vue
@@ -1,11 +1,13 @@
 <template>
-    <div>
-      <h1>Detail Hasil Diagnosa</h1>
+  <div class="container py-5">
+    <h1 class="text-center mb-4">Detail Hasil Diagnosa</h1>
+
+    <div class="table-responsive">
       <table class="table table-hover border">
         <thead class="thead-light">
           <tr>
-            <th>Gejala yang kamu alami saat ini</th>
-            <th>Tingkat keyakinan</th>
+            <th>Gejala yang Kamu Alami Saat Ini</th>
+            <th>Tingkat Keyakinan</th>
             <th>CF User</th>
           </tr>
         </thead>
@@ -17,19 +19,15 @@
           </tr>
         </tbody>
       </table>
-  
-      <div class="mt-4">
-        <h6 class="font-weight-bold">
-          Tabel perhitungan penyakit: <!---{{ $diagnosa['nama_penyakit'] }} ({{ $diagnosa['kode_penyakit'] }}) -->
-        </h6>
-      </div>
-  
-      <div class="mt-4" v-for="diagnosa in hasilDiagnosa" :key="diagnosa.kode_penyakit">
-        <div class="card card-body p-0 mt-5 border" style="box-shadow: none !important;">
-          <div class="card-header bg-primary text-white p-2">
-            <h6 class="font-weight-bold">Tabel perhitungan penyakit: {{ diagnosa.nama_penyakit }} ({{ diagnosa.kode_penyakit }})</h6>
-          </div>
-          <table class="table table-hover">
+    </div>
+
+    <div class="mt-4" v-for="diagnosa in hasilDiagnosa" :key="diagnosa.kode_penyakit">
+      <div class="card mt-5 border-0 shadow">
+        <div class="card-header bg-primary text-white">
+          <h6 class="font-weight-bold mb-0">Tabel Perhitungan Penyakit: {{ diagnosa.nama_penyakit }} ({{ diagnosa.kode_penyakit }})</h6>
+        </div>
+        <div class="card-body p-0">
+          <table class="table table-hover mb-0">
             <thead class="thead-light">
               <tr>
                 <th>Gejala</th>
@@ -55,65 +53,84 @@
           </table>
         </div>
       </div>
-  
-      <div class="mt-5">
-        <div class="alert alert-success">
-          <h5 class="font-weight-bold">Kesimpulan</h5>
-          <p>
-            Berdasarkan dari gejala yang kamu pilih atau alami juga berdasarkan
-            Role/Basis aturan yang sudah ditentukan oleh seorang pakar penyakit maka
-            perhitungan Algoritma Certainty Factor mengambil nilai CF yang paling
-            tinggi yakni
-            <span v-if="cfMax">
-    <b>{{ cfMax[1] }} dengan nilai CF {{ cfMax[0] }}</b>
-  </span>
-  <span v-else>
-    Tidak ada hasil diagnosa.
-  </span>
-            
-          </p>
-        </div>
-        <div class="mt-3 text-center">
-          <!-- {{-- <a href="{{ asset("storage/downloads/$riwayat->file_pdf") }}" target="_blank" class="btn btn-primary mr-1"><i class="fas fa-print mr-1"></i> Print</a>
-          <a href="{{ route('admin.diagnosa') }}" class="btn btn-warning mr-1"><i class="fas fa-redo mr-1"></i> Diagnosa ulang</a> --}} -->
-        </div>
-      </div>
     </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        gejalaTerpilih: [],
-        hasilDiagnosa: [],  
-        cfMax: null, // Tambahkan ini
-      };
-    },
-    
-    props: {
-      id: {
-        type: String,
-        required: true
-      }
-    },
-    mounted() {
-      this.fetchDetails();
-    },
-    methods: {
-      async fetchDetails() {
-        try {
-          const response = await axios.get(`riwayat/${this.id}`);
-          this.gejalaTerpilih = response.data.gejala_terpilih;
-          this.hasilDiagnosa = response.data.hasil_diagnosa;
-          this.cfMax = response.data.cf_max; // Tambahkan ini
-        } catch (error) {
-          console.error('Error fetching detail: ', error);
-          alert('Terjadi kesalahan saat mengambil detail diagnosa. Silakan coba lagi nanti.');
-        }
+
+    <!-- Kesimpulan -->
+    <div class="alert alert-success mt-5">
+      <h5 class="font-weight-bold">Kesimpulan</h5>
+
+      Berdasarkan dari gejala yang kamu pilih atau alami juga berdasarkan
+      Role/Basis aturan yang sudah ditentukan oleh seorang pakar penyakit maka
+      perhitungan Algoritma Certainty Factor mengambil nilai CF yang paling
+      tinggi yakni
+      <!-- Tampilkan hasil diagnosa -->
+      <!-- Jika ada cfMax -->
+      <span v-if="cfMax">
+        <!-- Tampilkan nama penyakit dan nilai CF -->
+        {{ cfMax[1] }} dengan nilai CF {{ cfMax[0] }}
+      </span>
+
+      <!-- Jika tidak ada cfMax -->
+      <!-- Tampilkan pesan bahwa tidak ada hasil diagnosa -->
+      <span v-else>Tidak ada hasil diagnosa.</span>
+
+    </div>
+
+  </div> <!-- Tutup div.container -->
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      gejalaTerpilih: [],
+      hasilDiagnosa: [],
+      cfMax: null,
+    };
+  },
+  props: {
+    id: {
+      type: String,
+      required: true
+    }
+  },
+  mounted() {
+    this.fetchDetails();
+  },
+  methods: {
+    async fetchDetails() {
+      try {
+        const response = await axios.get(`riwayat/${this.id}`);
+        this.gejalaTerpilih = response.data.gejala_terpilih;
+        this.hasilDiagnosa = response.data.hasil_diagnosa;
+        this.cfMax = response.data.cf_max;
+      } catch (error) {
+        console.error('Error fetching detail: ', error);
+        alert('Terjadi kesalahan saat mengambil detail diagnosa. Silakan coba lagi nanti.');
       }
     }
-  };
-  </script>
+  }
+};
+</script>
+
+<style scoped>
+
+/* Gaya untuk container */
+.container {
+  max-width: 800px;
+  margin: auto;
+}
+
+/* Gaya untuk card */
+.card {
+  border-radius: 0.5rem;
+}
+
+/* Gaya untuk card header */
+.card-header {
+  border-radius: 0.5rem 0.5rem 0 0;
+}
+
+</style>
--- src/views/content/diagnosis/hasil_diagnosis.vue
+++ src/views/content/diagnosis/hasil_diagnosis.vue
@@ -1,112 +1,116 @@
 <template>
-    <!-- Tampilan tabel data riwayat -->
-    <H1>Hasil Diagnosa</H1>
+  <div class="container py-5">
+    <h1 class="text-center mb-4">Hasil Diagnosa</h1>
     <div class="table-responsive">
       <table class="table table-hover border">
-        <!-- Header tabel -->
-        
-        <thead>
-          <th>ID</th>
-          <th>Penyakit terdiagnosa</th>
-          <th>Tanggal</th>
-          <th></th>
+        <thead class="thead-dark">
+          <tr>
+            <th>ID</th>
+            <th>Penyakit Terdiagnosa</th>
+            <th>Tanggal</th>
+            <th>Aksi</th>
+          </tr>
         </thead>
-        <!-- Data tabel -->
         <tbody>
           <tr v-for="data in sortedResults" :key="data.id">
             <td>{{ data.id }}</td>
-            <td>
-              <span v-html="getDiagnosedDisease(data)"></span>
-            </td>
+            <td><span v-html="getDiagnosedDisease(data)"></span></td>
             <td>{{ formatTanggal(data.created_at) }}</td>
-            <td><button @click="viewDetails(data.id)">View Details</button></td>
+            <td><button class="btn btn-primary" @click="viewDetails(data.id)">Lihat Detail</button></td>
           </tr>
         </tbody>
       </table>
     </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  import { mapActions } from 'vuex';
-  
-  // Gantilah URL API Anda dengan variabel lingkungan jika memungkinkan
-  const apiUrl = 'riwayat';
-  
-  function konversiArrayPHPkeJS(arrayPHP) {
-    try {
-      if (typeof arrayPHP === 'string') {
-        const diseaseMatches = arrayPHP.match(/"(.*?)"/g);
-        if (diseaseMatches) {
-          const diseases = diseaseMatches.map(match => match.replace(/"/g, ''));
-          return diseases;
-        }
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import { mapActions } from 'vuex';
+
+const apiUrl = 'riwayat';
+
+function konversiArrayPHPkeJS(arrayPHP) {
+  try {
+    if (typeof arrayPHP === 'string') {
+      const diseaseMatches = arrayPHP.match(/"(.*?)"/g);
+      if (diseaseMatches) {
+        const diseases = diseaseMatches.map(match => match.replace(/"/g, ''));
+        return diseases;
       }
-  
-      const parsedArray = JSON.parse(arrayPHP);
-      if (Array.isArray(parsedArray) && parsedArray.length > 1) {
-        return [parsedArray[1], parsedArray[0]]; // Hapus perkalian dengan 100 di sini
-      }
-    } catch (error) {
-      console.error('Error parsing cf_max data:', error);
     }
-    return ['Tidak ada penyakit yang didiagnosis', 0];
+
+    const parsedArray = JSON.parse(arrayPHP);
+    if (Array.isArray(parsedArray) && parsedArray.length > 1) {
+      return [parsedArray[1], parsedArray[0]];
+    }
+  } catch (error) {
+    console.error('Error parsing cf_max data:', error);
   }
-  
-  export default {
-    data() {
-      return {
-        results: []
-      };
-    },
-    computed: {
+  return ['Tidak ada penyakit yang didiagnosis', 0];
+}
+
+export default {
+  data() {
+    return {
+      results: []
+    };
+  },
+  computed: {
     sortedResults() {
       return this.results.slice().sort((a, b) => a.id - b.id);
     }
   },
-    mounted() {
-      this.fetchData();
-    },
-    methods: {
-      async fetchData() {
-        try {
-          const response = await axios.get(apiUrl);
-          this.results = response.data.data;
-        } catch (error) {
-          console.error('Error fetching data: ', error);
-          alert('Terjadi kesalahan saat mengambil data. Silakan coba lagi nanti.');
-        }
-      },
-     
-      viewDetails(id) {
-        mapActions('diagnosis', ['detail_diagnosis']),
-        this.$router.push({ name: 'detail_diagnosis', params: { id } });
-      },
-      getDiagnosedDisease(data) {
-        if (data.cf_max) {
-          const [disease, percentage] = konversiArrayPHPkeJS(data.cf_max);
-  
-          if (percentage !== undefined) {
-            const formattedPercentage = (percentage * 100).toFixed(2);
-            return `${disease} (${formattedPercentage}%)`;
-          }
-  
-          return `${disease}`;
-        }
-  
-        return 'Tidak ada penyakit yang didiagnosis';
-      },
-      formatTanggal(tanggal) {
-        return new Date(tanggal).toLocaleString('en-US', {
-          day: '2-digit',
-          month: 'short',
-          year: 'numeric',
-          hour: '2-digit',
-          minute: '2-digit',
-          second: '2-digit'
-        });
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    ...mapActions('diagnosis', ['detail_diagnosis']),
+    async fetchData() {
+      try {
+        const response = await axios.get(apiUrl);
+        this.results = response.data.data;
+      } catch (error) {
+        console.error('Error fetching data: ', error);
+        alert('Terjadi kesalahan saat mengambil data. Silakan coba lagi nanti.');
       }
+    },
+   
+    viewDetails(id) {
+      this.detail_diagnosis(id);
+      this.$router.push({ name: 'detail_diagnosis', params: { id } });
+    },
+    getDiagnosedDisease(data) {
+      if (data.cf_max) {
+        const [disease, percentage] = konversiArrayPHPkeJS(data.cf_max);
+
+        if (percentage !== undefined) {
+          const formattedPercentage = (percentage * 100).toFixed(2);
+          return `${disease} (${formattedPercentage}%)`;
+        }
+
+        return `${disease}`;
+      }
+
+      return 'Tidak ada penyakit yang didiagnosis';
+    },
+    formatTanggal(tanggal) {
+      return new Date(tanggal).toLocaleString('en-US', {
+        day: '2-digit',
+        month: 'short',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+      });
     }
-  };
-  </script>
-  
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  max-width: 800px;
+  margin: auto;
+}
+</style>
