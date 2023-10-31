658f57898c3b05d49421cb4207ee08ca86c0ed55
anti009x <riskisuleman76@gmail.com>
2023-10-31 10:34:28 +0700

    Perbaharuan Fitur Dashboard Diagnosa

--- src/views/content/diagnosis/indexdiagnosis.vue
+++ src/views/content/diagnosis/indexdiagnosis.vue
@@ -1,60 +1,68 @@
 <template>
-  <div class="text-center">
-    <h2>Diagnosa Penyakit</h2>
 
-    <a href="/hasil_diagnosis">Check Hasil Diagnosa</a>
-    
-    <!-- Error Alert -->
-    <div v-if="error" class="alert alert-danger mx-auto" style="max-width: 600px;">{{ error }}</div>
-    
-    <section class="row">
-      <!-- chart section -->
-      <div class="col-md-12">
-        <div class="card mx-auto" style="max-width: 800px;">
-          <div class="card-body">
-            <form @submit.prevent="submitForm">
 
-              <!-- <label for="nama"><b><i class="fas fa-user mr-1"></i> Nama</b></label>
-              <input type="text" class="form-control mb-3 mx-auto" style="max-width: 300px;" v-model="nama"> -->
+<body>
+  <div class="stars">
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+  <div class="star"></div>
+</div>
+  <div class="container py-5 ">
 
-              <p>Pilih gejala yang sedang dirasakan.</p>
-              <label for=""><b><i class="fas fa-th mr-1"></i> Gejala-gejala</b></label>
+    <h2 class="text-center mb-4 fw-semibold">DIAGNOSA PENYAKIT</h2>
 
-          
-              <div class="row">
-                
-                <div class="col-md-4 mx-auto border-data border-container" v-for="(item, index) in gejala" :key="index">
-    <div class=" border-container">
-        <span class="ml-2">{{ item.nama }}</span>
-        <select v-model="diagnosa[item.id]" 
-                class="form-control form-control-sm select-control" 
-                :class="getBorderColor(diagnosa[item.id])">
-            <option :value="`${item.id}+-1`">Pasti tidak</option>
-            <option :value="`${item.id}+-0.8`">Hampir pasti tidak</option>
-            <option :value="`${item.id}+-0.6`">Kemungkinan besar tidak</option>
-            <option :value="`${item.id}+-0.4`">Mungkin tidak</option>
-            <option value="" selected>Tidak tahu</option>
-            <option :value="`${item.id}+0.4`">Mungkin</option>
-            <option :value="`${item.id}+0.6`">Sangat mungkin</option>
-            <option :value="`${item.id}+0.8`">Hampir pasti</option>
-            <option :value="`${item.id}+1`">Pasti</option>
-        </select>
+    <div class="text-center mb-3 ">
+      <router-link to="/hasil_diagnosis" class="btn btn-primary btn-sm btn-outline-dark  " style="color:white;box-shadow: 2px 2px black; ">Lihat Hasil Diagnosa</router-link>
     </div>
-</div>
-</div>
 
-              <div class="mt-3">
-                <button type="submit" class="btn btn-primary" :disabled="isLoading">Diagnosa sekarang</button>
+    <!-- Error Alert -->
+    <div v-if="error" class="alert alert-danger">{{ error }}</div>
+
+    <div class="card ">
+      <div class="card-body" >
+        <form @submit.prevent="submitForm">
+          <p > Pilih gejala yang sedang dirasakan.</p>
+          <div class="row">
+            <div class="col-md-6 " style="font-family: cursive;" v-for="(item, index) in gejala" :key="index">
+              <div class="form-group ">
+                <label><b>{{ item.nama }}</b>
+                </label>
+                <select v-model="diagnosa[item.id]" class="form-control select-control" 
+                        :class="getBorderColor(diagnosa[item.id])">
+                  <option :value="null" disabled :selected="!diagnosa[item.id]">Pilihlah Opsi Gejala</option>
+                  <option :value="`${item.id}+-1`" >Pasti tidak</option>
+                  <option :value="`${item.id}+-0.8`">Hampir pasti tidak</option>
+                  <option :value="`${item.id}+-0.6`">Kemungkinan besar tidak</option>
+                  <option :value="`${item.id}+-0.4`">Mungkin tidak</option>
+                  <option value="" selected>Tidak tahu</option>
+                  <option :value="`${item.id}+0.4`">Mungkin</option>
+                  <option :value="`${item.id}+0.6`">Sangat mungkin</option>
+                  <option :value="`${item.id}+0.8`">Hampir pasti</option>
+                  <option :value="`${item.id}+1`">Pasti</option>
+                </select>
               </div>
-
-            </form>
+            </div>
           </div>
-        </div>
-      </div>
-    </section>
-  </div>
-</template>
 
+          <div class="text-center mt-3">
+            <button type="submit" class="btn btn-primary  btn-outline-dark  " style="color:white;box-shadow: 2px 2px black; " :disabled="isLoading">Diagnosa sekarang</button>
+          </div>
+
+        </form>
+      </div>
+    </div>
+
+  </div>
+</body>    
+</template>
 
 <script>
 import axios from 'axios';
@@ -62,7 +70,6 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      // nama: '',
       gejala: [],
       diagnosa: {},
       error: null,
@@ -74,7 +81,7 @@ export default {
       axios.get('/diagnosa').then(response => {
         this.gejala = response.data;
         this.gejala.forEach(item => {
-          this.$set(this.diagnosa, item.id, '');
+          this.$set(this.diagnosa, item.id, null);
         });
       }).catch(error => {
         console.log(error);
@@ -83,65 +90,225 @@ export default {
     submitForm() {
       this.isLoading = true;
       axios.post('diagnosa', {
-      nama: '',
         diagnosa: Object.values(this.diagnosa)
       }).then(response => {
         this.isLoading = false;
-        this.response = response('Succes');
-        // this.$router.push({ name: 'hasil_riwayat' });
+        this.$router.push({ name: 'hasil_riwayat' });
       }).catch(error => {
         this.isLoading = false;
         this.error = "Data Terkirim , Bisa Langsung Check Hasil Diagnosa.Jika Datanya Belum Terkirim Harap Kirim Ulang";
-        // this.$router.push({ name: 'hasil_riwayat' });
       });
     },
     getBorderColor(value) {
-      return value === "" ? 'red-border' : 'green-border';
+      return value === "" ? 'border-danger' : 'border-success';
     }
   },
   mounted() {
     this.loadGejala();
   }
 }
+
 </script>
 
-<style scoped>
-.red-border {
-  border: 1px solid rgba(227, 39, 79, .8);
-}
-.green-border {
-  border: 1px solid rgba(50, 179, 104, .8);
-}
+<style  scoped>
 
-.select-control {
-    margin-right: 4rem;
-    border-radius: 0.25rem;
-    transition: all 0.3s ease;
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-    border: 1px solid #d1d5db;
-    align-items: center;
-
+  
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background: radial-gradient(ellipse at bottom, #0d1d31 0%, #0c0d13 100%);
+  overflow: hidden;
 }
 
+.stars {
 
-.select-control:hover {
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+
+  transform: rotate(-45deg);
 }
 
+.star {
+  --star-color: #02b7ff;
+  --star-tail-length: 6em;
+  --star-tail-height: 2px;
+  --star-width: calc(var(--star-tail-length) / 6);
+  --fall-duration: 9s;
+  --tail-fade-duration: var(--fall-duration);
+  position: absolute;
+  top: var(--top-offset);
+  left: 0;
+  width: var(--star-tail-length);
+  height: var(--star-tail-height);
+  color: var(--star-color);
+  background: linear-gradient(45deg, currentColor, transparent);
+  border-radius: 50%;
+  filter: drop-shadow(0 0 6px currentColor);
+  transform: translate3d(104em, 0, 0);
+  animation: fall var(--fall-duration) var(--fall-delay) linear infinite, tail-fade var(--tail-fade-duration) var(--fall-delay) ease-out infinite;
+}
+@media screen and (max-width: 750px) {
+  .star {
+    animation: fall var(--fall-duration) var(--fall-delay) linear infinite;
+  }
+}
+.star:nth-child(1) {
+  --star-tail-length: 5.03em;
+  --top-offset: 59.93vh;
+  --fall-duration: 9.626s;
+  --fall-delay: 8.996s;
+}
+.star:nth-child(2) {
+  --star-tail-length: 7.2em;
+  --top-offset: 71.04vh;
+  --fall-duration: 11.112s;
+  --fall-delay: 3.405s;
+}
+.star:nth-child(3) {
+  --star-tail-length: 7.18em;
+  --top-offset: 76.01vh;
+  --fall-duration: 11.91s;
+  --fall-delay: 0.859s;
+}
+.star:nth-child(4) {
+  --star-tail-length: 5.14em;
+  --top-offset: 52.36vh;
+  --fall-duration: 6.971s;
+  --fall-delay: 8.571s;
+}
+.star:nth-child(5) {
+  --star-tail-length: 6.69em;
+  --top-offset: 14.99vh;
+  --fall-duration: 7.856s;
+  --fall-delay: 9.288s;
+}
+.star:nth-child(6) {
+  --star-tail-length: 5.98em;
+  --top-offset: 22vh;
+  --fall-duration: 8.507s;
+  --fall-delay: 7.882s;
+}
+.star:nth-child(7) {
+  --star-tail-length: 5.37em;
+  --top-offset: 26.56vh;
+  --fall-duration: 8.907s;
+  --fall-delay: 1.577s;
+}
+.star:nth-child(8) {
+  --star-tail-length: 7.5em;
+  --top-offset: 56.58vh;
+  --fall-duration: 7.118s;
+  --fall-delay: 2.689s;
+}
+.star:nth-child(9) {
+  --star-tail-length: 5.35em;
+  --top-offset: 67.4vh;
+  --fall-duration: 7.911s;
+  --fall-delay: 5.894s;
+}
+.star:nth-child(10) {
+  --star-tail-length: 7.45em;
+  --top-offset: 95.77vh;
+  --fall-duration: 11.931s;
+  --fall-delay: 1.085s;
+}
+.star:nth-child(11) {
+  --star-tail-length: 7.48em;
+  --top-offset: 21.96vh;
+  --fall-duration: 9.074s;
+  --fall-delay: 7.508s;
+}
+.star:nth-child(12) {
+  --star-tail-length: 6.93em;
+  --top-offset: 94.48vh;
+  --fall-duration: 8.435s;
+  --fall-delay: 9.398s;
+}
+.star:nth-child(13) {
+  --star-tail-length: 6em;
+  --top-offset: 49.1vh;
+  --fall-duration: 8.1s;
+  --fall-delay: 9.61s;
+}
+.star:nth-child(14) {
+  --star-tail-length: 7.37em;
+  --top-offset: 11.43vh;
+  --fall-duration: 8.823s;
+  --fall-delay: 6.682s;
+}
+.star::before, .star::after {
+  position: absolute;
+  content: "";
+  top: 0;
+  left: calc(var(--star-width) / -2);
+  width: var(--star-width);
+  height: 100%;
+  background: linear-gradient(45deg, transparent, currentColor, transparent);
+  border-radius: inherit;
+  animation: blink 2s linear infinite;
+}
+.star::before {
+  transform: rotate(45deg);
+}
+.star::after {
+  transform: rotate(-45deg);
+}
+
+@keyframes fall {
+  to {
+    transform: translate3d(-30em, 0, 0);
+  }
+}
+@keyframes tail-fade {
+  0%, 50% {
+    width: var(--star-tail-length);
+    opacity: 1;
+  }
+  70%, 80% {
+    width: 0;
+    opacity: 0.4;
+  }
+  100% {
+    width: 0;
+    opacity: 0;
+  }
+}
+@keyframes blink {
+  50% {
+    opacity: 0.6;
+  }
+}
+
+
+
+.form-group option {
+  font-family: cursive;
+
+}
+
+  h2{
+    text-shadow: 1px 1px #0026ff;
+  color: rgb(255, 255, 255);
+  font-family: cursive;
+  }
+
+.container {
+  max-width: 800px;
+  margin: auto;
+}
+
+p{
+  font-weight: bold;
+  font-family:cursive;
+}
 .select-control:focus {
-    outline: none;
-    border-color: #007BFF;
-    box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.5);
+  outline: none;
+  box-shadow: none !important;
 }
-
-.border-container {
-    border-radius: 0.25rem;
-    transition: all 0.3s ease;
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+.border-danger {
+  border-color: #dc3545 !important;
 }
-
-.border-container:hover {
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+.border-success {
+  border-color: #28a745 !important;
 }
-
 </style>
